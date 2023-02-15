c0e37ab5be89a5eb2bf797361f3d0cb2078690a5
Sergio Gregorutti <sergiogregorutti@gmail.com>
2023-02-15 15:17:55 -0300

    Create useEffect example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 
 import moment from 'moment';
 
+function Texto() {
+  useEffect(() => {
+    console.log('Componente Texto montado');
+
+    return () => {
+      console.log('Componente Texto desmontado');
+    };
+  }, []);
+
+  return (
+    <p>Texto de ejemplo</p>
+  )
+}
+
 function App() {
+  console.log('El componente App ha sido renderizado');
   const [time, setTime] = useState(moment(Date.now()).format('DD/MM/YYYY H:m:s'));
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    console.log('El tiempo ha sido actualizado');
+  }, [time]);
 
   const updateTime = () => {
     setTime(moment(Date.now()).format('DD/MM/YYYY H:m:s'));
+  };
+
+  const toggleShowText = () => {
+    setShowText(!showText);
   };
 
   return (
@@ -17,7 +41,11 @@ function App() {
       <ItemListContainer greeting={'Bienvenidos'} />
 
       <p>Fecha y Hora: {time}</p>
-      <button onClick={updateTime}>Actualizar</button>
+      <button onClick={updateTime} style={{marginRight: '15px'}}>Actualizar Fecha y Hora</button>
+
+      <button onClick={toggleShowText}>Mostrar/Ocultar Texto</button>
+
+      {showText && <Texto />}
     </div>
   );
 }
