cebcafb3a4c7525bf84b4573775577a2b60a5fb8
Sergio Gregorutti <sergiogregorutti@gmail.com>
2023-02-27 18:30:34 -0300

    Implement Fetch example

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      console.log('json', json);
+      setUsers(json.data);
+    });
+  }, []);
+
   return (
     <div className="container">
       <NavBar />
       <ItemListContainer greeting={'Bienvenidos'} />
+
+      <h2>Usuarios</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <img src={user.avatar} />
+            <h4>{`${user.first_name} ${user.last_name}`}</h4>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
